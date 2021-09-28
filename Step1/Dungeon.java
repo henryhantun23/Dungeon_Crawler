ba607c9913e23c48d89b2b821defc5acbe99ad74
bolen2 <54512766+bolen2@users.noreply.github.com>
2021-09-28 15:42:41 -0400

    Added classes

---
+++ Step1/Dungeon.java
@@ -0,0 +1,10 @@
+package Step1;
+
+public class Dungeon {
+    private String name;
+    private int width;
+    private int topHeight;
+    private int gameHeight;
+    private int bottomHeight;
+    
+}
--- Step1/Step1XMLHandler.java
+++ Step1/Step1XMLHandler.java
@@ -6,10 +6,16 @@ import org.xml.sax.helpers.DefaultHandler;
 public class Step1XMLHandler extends DefaultHandler {
 
     private static final int DEBUG = 1;
-    private static final String CLASSID = "StudentXMLHandler";
+    private static final String CLASSID = "Step1XMLHandler";
 
     private StringBuilder data = null;
 
-    
+    public void characters(char ch[], int start, int length) throws SAXException {
+        data.append(new String(ch, start, length));
+        if (DEBUG > 1) {
+            System.out.println(CLASSID + ".characters: " + new String(ch, start, length));
+            System.out.flush();
+        }
+    }
     
 }
