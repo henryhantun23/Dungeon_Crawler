0a5f2cbb6eff2eff79827a5cf44547be3e39dccf
bolen2 <54512766+bolen2@users.noreply.github.com>
2021-09-29 13:39:00 -0400

    Fixing xml handler

--- Step1/Step1XMLHandler.java
+++ Step1/Step1XMLHandler.java
@@ -1,6 +1,9 @@
 package Step1;
 
-import Step1.displayable.*;
+import Step1.displayable.Structure.Room;
+import Step1.displayable.Dungeon;
+import Step1.displayable.creatures.*;
+import Step1.action.*;
 import java.util.ArrayList;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -13,18 +16,84 @@ public class Step1XMLHandler extends DefaultHandler {
 
     private StringBuilder data = null;
 
-    private ArrayList<Dungeon> dungeons = new ArrayList<Dungeon>();
+    private Dungeon[] dungeons;
+    private int maxDungeons = 0;
+    private int dungeonCount = 0;
 
     private Dungeon dungeonBeingParsed = null;
     private Room roomBeingParsed = null;
+    private Monster monsterBeingParsed = null;
+    private Player playerBeingParsed = null;
+    private Action creatureActionBeingParsed = null;
 
+    private boolean bPosX = false;
+    private boolean bPosY = false;
+    private boolean bWidth = false;
+    private boolean bHeight = false;
+    private boolean bType = false;
+    private boolean bHp = false;
+    private boolean bMaxhit = false;
+    private boolean bVisible = false;
+    private boolean bHpMoves = false;
+    private boolean bActionMessage = false;
 
-    public void characters(char ch[], int start, int length) throws SAXException {
-        data.append(new String(ch, start, length));
-        if (DEBUG > 1) {
-            System.out.println(CLASSID + ".characters: " + new String(ch, start, length));
-            System.out.flush();
-        }
+    public Dungeon[] getDungeons(){
+        return dungeons;
     }
+
+    public Step1XMLHandler(){
+
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+        if (DEBUG > 1) {
+            System.out.println(CLASSID + ".startElement qName: " + qName);
+        }
+
+        if (qName.equalsIgnoreCase("Dungeon")) {
+            String name = attributes.getValue("name");
+            int width = Integer.parseInt(attributes.getValue("width"));
+            int topHeight = Integer.parseInt(attributes.getValue("topHeight"));
+            int gameHeight = Integer.parseInt(attributes.getValue("gameHeight"));
+            int bottomHeight = Integer.parseInt(attributes.getValue("bottomHeight"));
+
+        } else if (qName.equalsIgnoreCase("Rooms")) {
+
+        } else if (qName.equalsIgnoreCase("Room")) {
+
+        } else if (qName.equalsIgnoreCase("Monster")) {
+
+        } else if (qName.equalsIgnoreCase("Player")) {
+
+        } else if (qName.equalsIgnoreCase("CreatureAction")) {
+
+        } else if (qName.equalsIgnoreCase("posX")) {
+            bPosX = true;
+        } else if (qName.equalsIgnoreCase("posY")) {
+            bPosY = true;
+        } else if (qName.equalsIgnoreCase("width")) {
+            bWidth = true;
+        } else if (qName.equalsIgnoreCase("height")) {
+            bHeight = true;
+        } else if (qName.equalsIgnoreCase("type")) {
+            bType = true;
+        } else if (qName.equalsIgnoreCase("hp")) {
+            bHp = true;
+        } else if (qName.equalsIgnoreCase("maxhit")) {
+            bMaxhit = true;
+        } else if (qName.equalsIgnoreCase("visible")) {
+            bVisible = true;
+        } else if (qName.equalsIgnoreCase("hpMoves")) {
+            bHpMoves = true;
+        } else if (qName.equalsIgnoreCase("actionMessage")) {
+            bActionMessage = true;
+        } else {
+            System.out.println("Unknown qname: " + qName);
+        }
+        data = new StringBuilder();
+    }
+
     
 }
