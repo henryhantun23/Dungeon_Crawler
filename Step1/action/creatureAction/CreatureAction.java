39281211a52eab3f4b2275e6095a1a3dc8189e43
bolen2 <54512766+bolen2@users.noreply.github.com>
2021-10-08 14:45:59 -0400

    reset

--- Step1/action/creatureAction/CreatureAction.java
+++ Step1/action/creatureAction/CreatureAction.java
@@ -6,12 +6,8 @@ import Step1.displayable.creatures.Creature;
 public class CreatureAction extends Action{
     private String action_name;
     private String action_type;
+    private String message;
 
-<<<<<<< Updated upstream
-    public CreatureAction(Creature owner){
-        System.out.println("Creating endgame player action");
-        setMessage("Player is killed!");
-=======
     public CreatureAction(String _action_name, String _action_type){
         action_name = _action_name;
         action_type = _action_type;
@@ -24,7 +20,6 @@ public class CreatureAction extends Action{
     // public CreatureAction(Creature owner){
     //     System.out.println("Creating endgame player action");
     //     setMessage("Player is killed!");
->>>>>>> Stashed changes
 
     }
 }
