c98d58e0c043ccbb40275e1e8fe9b1f78c6f5f67
bolen2 <54512766+bolen2@users.noreply.github.com>
2021-11-09 15:09:44 -0500

    editing packages

--- src/game/rogue.java
+++ src/game/rogue.java
@@ -1,12 +1,11 @@
 package game;
 
-
-
 import java.io.File;
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+
 
 import org.xml.sax.SAXException;
 
